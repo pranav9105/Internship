08debbee17675ae54b508bfc38debe578e117638
Pranav Desai <pranavd041@gmail.com>
2026-01-11 08:54:46 +0000

    https://www.heart.org/en/-/media/AHA/H4GM/Article-Images/travel.jpg?sc_l

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.heart.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
