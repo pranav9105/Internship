0f4729fa71045a52c91f3fa3a95459d56b26ec87
Pranav Desai <pranavd041@gmail.com>
2026-01-14 11:11:08 +0000

    https://media.assettype.com/outlooktraveller%2F2024-09-20%2Frsefsl0l%2Fs

--- next.config.ts
+++ next.config.ts
@@ -131,6 +131,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.assettype.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   env: {
