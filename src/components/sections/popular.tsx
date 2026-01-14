64a97aa16981d37a2ed976009b213e5fb329c96f
Pranav Desai <pranavd041@gmail.com>
2026-01-14 10:59:15 +0000

    add all  29 states of india in it

--- src/components/sections/popular.tsx
+++ src/components/sections/popular.tsx
@@ -17,7 +17,34 @@ const domesticCities = [
 ];
 
 const regions = [
-    'Goa', 'Kerala', 'Rajasthan', 'Himachal Pradesh', 'Uttarakhand',
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal"
 ];
 
 const countries = [
